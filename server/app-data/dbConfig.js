c06e855f2bca01dd7145bde1bb9dc6769e8d70c5
Tapan Prajapati <tapanprajapati24598@gmail.com>
2025-10-02 16:29:40 -0700

    MariaDB compatible

--- server/app-data/dbConfig.js
+++ server/app-data/dbConfig.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DATABASE,
-  port: process.env.RDS_PORT,
-  ssl: true,
-  connectionLimit: process.env.RDS_CONNECTION_LIMIT
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "personalaccount",
+  port: process.env.DB_PORT || "3306",
+  connectionLimit: process.env.DB_CONNECTION_LIMIT || "15"
 };
+
