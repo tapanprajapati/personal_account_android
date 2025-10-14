d918fcc9e654840d3c733b7db5be620d2b3d1d9c
Tapan Prajapati <tapan.prajapati24598@gmail.com>
2025-10-13 21:45:14 -0700

    Fix server address

--- personal-account-web/src/databasehandler/WebStorage.js
+++ personal-account-web/src/databasehandler/WebStorage.js
@@ -9,7 +9,7 @@ export default class WebStorage {
         console.log("Initializing Web Storage");
 
         WebStorage._instance = this;
-        this.IP = "untensile-carla-unentrapped.ngrok-free.dev";
+        this.IP = "https://untensile-carla-unentrapped.ngrok-free.dev";
         this.user = "";
         this.KEYS = {
             IP: "IP",
