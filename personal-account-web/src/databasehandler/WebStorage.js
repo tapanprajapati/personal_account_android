edc1ea2891fed59e8a77860b8f4eb71e0049770d
Tapan Prajapati <tapan.prajapati24598@gmail.com>
2025-11-06 10:40:29 -0800

    Fix API IP

--- personal-account-web/src/databasehandler/WebStorage.js
+++ personal-account-web/src/databasehandler/WebStorage.js
@@ -9,7 +9,7 @@ export default class WebStorage {
         console.log("Initializing Web Storage");
 
         WebStorage._instance = this;
-        this.IP = "http://localhost:8081";
+        this.IP = "https://untensile-carla-unentrapped.ngrok-free.dev";
         this.user = "";
         this.KEYS = {
             IP: "IP",
