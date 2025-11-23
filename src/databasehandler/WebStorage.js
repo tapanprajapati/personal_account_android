5e838de33d04e2cd4c9e994b72ae2939cbf75777
Tapan Prajapati <tapan.prajapati24598@gmail.com>
2025-11-22 19:18:15 -0800

    Merge from gcp

--- src/databasehandler/WebStorage.js
+++ src/databasehandler/WebStorage.js
@@ -9,7 +9,7 @@ export default class WebStorage {
         console.log("Initializing Web Storage");
 
         WebStorage._instance = this;
-        this.IP = "https://untensile-carla-unentrapped.ngrok-free.dev";
+        this.IP = "http://10.0.0.172:8081";
         this.user = "";
         this.KEYS = {
             IP: "IP",
